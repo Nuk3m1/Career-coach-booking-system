4b12bfd3c1070481ddb1c0680d379f1bc76db437
Nuk3m1 <140965656+Nuk3m1@users.noreply.github.com>
2026-02-05 22:45:19 +0800

    Update TestInterface.sql

--- SQL/TestInterface.sql
+++ SQL/TestInterface.sql
@@ -3,12 +3,12 @@ VALUES ('001', '张三', 'zhangsan@example.com');
 
 INSERT INTO `booking` (booking_id, user_id, coach_name, coach_email, start_time, end_time, status, cancel_url)
 VALUES (
-           '123',
+           '100',
            '001',
            '导师 A',
            'coachA@cal.com',
            '2026-02-10 10:00:00',
            '2026-02-10 10:30:00',
-           'BOOKING_CREATED',
+           'PENDING',
            'https://cal.com/reschedule/mock_token'
        );
